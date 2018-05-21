97c00ae79e903a09dd3215063b89fc69e70ea355
yarwest <yarnoboelens@gmail.com>
2018-05-21 12:31:34 +0200

    Created basic form creation based on form model.

---
+++ src/main/webapp/js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(
+	$.get({
+		url: "/form/",
+		success: function(data) {
+			data.components.forEach(function (component) {
+				switch (component.type) {
+					case "PASSWORD":
+						$("<div>").load("/components/passwordFormField.html", function() {
+							$("#formBody").append($(this).html());
+						});
+						break;
+					case "TEXT":
+					default:
+						$("<div>").load("/components/textFormField.html", function() {
+							$("#formBody").append($(this).html());
+						});
+						break;
+				}
+			});
+		}}
+	)
+);
